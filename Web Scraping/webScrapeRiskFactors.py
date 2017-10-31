033b0587bd25dc7b7a2535a8afea4afef7557d3c
Reed Thunstrom <rsthunstrom@gmail.com>
2017-10-30 22:04:19 -0500

    Update webScrapeRiskFactors.py
    updating for all nuances of SEC filing text

--- Web Scraping/webScrapeRiskFactors.py
+++ Web Scraping/webScrapeRiskFactors.py
@@ -9,15 +9,15 @@ from __future__ import division, print_function
 import pandas as pd
 import requests  # functions for interacting with web pages
 from bs4 import BeautifulSoup  # DOM html manipulation
-import pandas as pd
 from pyparsing import (makeHTMLTags, SkipTo, commonHTMLEntity, replaceHTMLEntity, 
     htmlComment, anyOpenTag, anyCloseTag, LineEnd, OneOrMore, replaceWith)
 from pyparsing import ParserElement
 
 #import text file of 10 companies from 2017-06-30 report date and 2016-06-30 report date
 #This will be an input from web scraping DF in Michelle's web scrape code
-
-rdd = pd.read_excel('/Users/z013nx1/Downloads/webScrapeURL.xlsx') #import text file with pipe delimiter
+#proj15output4
+rdd = pd.read_excel('/Users/z013nx1/Downloads/Project/proj15aoutput4.xlsx') #import text file with pipe delimiter
+#rdd = pd.read_excel('/Users/z013nx1/Downloads/webScrapeURL.xlsx') #import text file with pipe delimiter
 df = pd.DataFrame(rdd) #convert to pandas df
 
 
@@ -37,13 +37,20 @@ for i in range(len(df)):
     repeatedNewlines = LineEnd() + OneOrMore(LineEnd())
     repeatedNewlines.setParseAction(replaceWith("\n\n"))
     secondPass = repeatedNewlines.transformString(firstPass) #remove additional HTML tags
-    allsoup.append(str(secondPass))
+    allsoup.append(str(secondPass.lower()))
     
 df['Soup'] = allsoup #create new column from scraped and cleaned HTML data
 
 #create boolean for Risk Factors
 #need to determine which keywords we will use to parse the file
-df['RiskFactors'] = df['Soup'].str.contains('ITEM 1A.RISK FACTORS', regex=True) 
+df['RiskFactors'] = df['Soup'].str.contains('risk factors', regex=True)
+
+df['Cautionary1'] = df['Soup'].str.contains('forward-looking and cautionary statements', regex=True)
+
+df['Cautionary2'] = df['Soup'].str.contains('forward looking and cautionary statements', regex=True)
+
+df['unregistered'] = df['Soup'].str.contains('unregistered sales of equity', regex=True)
+
 
 RFText = [] #empty list for risk factors after they are parsed
         
@@ -54,17 +61,44 @@ RFText = [] #empty list for risk factors after they are parsed
 #we collect after the start with [1] and before the end with [0]
 
 for i in range(len(df)):
-    if df.ix[i, 'RiskFactors']==True:
-        start = 'ITEM 1A.RISK FACTORS'
-        end = 'ITEM 2.'
+    
+    if df.ix[i, 'Cautionary2']==True:
+        
+        start = 'item 4.'
+        end = 'cautionary statements'
         s = df.ix[i, 'Soup']
-        rf = (s.split(start))[1].split(end)[0]
+        rf = (s.split(start))[1].split(end)[-1]
         RFText.append(rf)
+        print('caution2', i)
+        
+    elif df.ix[i, 'Cautionary1']==True:
+        
+        start = 'item 4.'
+        end = 'cautionary statements'
+        s = df.ix[i, 'Soup']
+        rf = (s.split(start))[1].split(end)[-1]
+        RFText.append(rf)
+        print('caution1', i)
+        
+    elif df.ix[i, 'RiskFactors']==True:
+        start = 'unregistered sales of equity'
+        end = 'risk factors'
+        s = df.ix[i, 'Soup']
+        
+        if df.ix[i, 'unregistered'] == True:
+            if len(s.split(start)) == 2:
+                rf = (s.split(start))[0].split(end)[-1]
+                RFText.append(rf)
+                print('RF length one with unreg', i)
+            else:
+                rf = (s.split(start))[1].split(end)[-1]
+                RFText.append(rf)
+                print('RF length many with unreg', i)
+        else:
+            rf = (s.split(end))[-1]
+            RFText.append(rf)
+            print('RF no unreg', i)  
     else:
-        start = 'Cautionary Statements'
-        end = 'Item 4.'
-        s = df.ix[i, 'Soup']
-        rf = (s.split(start))[1].split(end)[0]
-        RFText.append(rf)
-  
+        RFText.append('NO RISK FACTORS')
+        
 df['RiskFactorText'] = RFText #create new column from risk factors
